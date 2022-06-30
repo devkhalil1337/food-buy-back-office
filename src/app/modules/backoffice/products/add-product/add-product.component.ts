4d119abe4710f8235084d0b95f4575858ca432f0
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2022-07-01 03:33:25 +0500

    Add product module updated

--- src/app/modules/backoffice/products/add-product/add-product.component.ts
+++ src/app/modules/backoffice/products/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
+  filePath:string;
+  productForm:FormGroup
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initlizeProductForm();
+  }
+
+  initlizeProductForm():void {
+    this.productForm = new FormGroup({
+      productName: new FormControl("",Validators.required),
+      cateId: new FormControl("",Validators.required),
+      productPrice: new FormControl("",Validators.required),
+      productDescription: new FormControl(""),
+      productImage: new FormControl(""),
+      productStatus: new FormControl("",Validators.required),
+    })
+  }
+
+  onProductAdd(){
+    console.log(this.productForm.value);
+  }
+
+  imagePreview(e) {
+    const file = (e.target as HTMLInputElement).files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.filePath = reader.result as string;
+    }
+    reader.readAsDataURL(file)
   }
 
 }
