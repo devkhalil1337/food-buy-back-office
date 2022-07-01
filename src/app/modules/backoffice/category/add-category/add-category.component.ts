ecb17102f3bf41b1e602583e08e0ea3f0656774e
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2022-07-02 03:52:56 +0500

    Add category is updated

--- src/app/modules/backoffice/category/add-category/add-category.component.ts
+++ src/app/modules/backoffice/category/add-category/add-category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCategoryComponent implements OnInit {
 
+
+  categoryForm:FormGroup;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.categoryForm = new FormGroup({
+      categoryName: new FormControl("",Validators.required),
+      categoryDetails: new FormControl(""),
+      categorySortBy : new FormControl("",[Validators.required,Validators.min(0)]),
+      CreationDate :new FormControl(""),
+      active: new FormControl("1",Validators.required)
+    })
+  }
+
+
+  onSubmit(){
+    console.log(this.categoryForm.value);
   }
 
 }
