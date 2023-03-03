41e4b33ae4135d19f12c48601d64ee08ab260927
devkhalil1337 <devkhalil1337@gmail.com>
2023-03-03 23:02:03 +0500

    Gross sales graph API integrated

--- src/app/modules/backoffice/dashboard/dashboard.service.ts
+++ src/app/modules/backoffice/dashboard/dashboard.service.ts
@@ -23,5 +23,9 @@ export class DashboardService {
   getCancelledOrders(){
     return this.apiService.request("get",`ReportingDashboard/GetNumberOfOrders?orderStatus=cancelled&Datefrom=2023-01-01&Dateto=2023-03-01`);
   }
+
+  getNetSalesForGraph(){
+    return this.apiService.request("get",`ReportingDashboard/GetGrossSalesByDay?Datefrom=2023-01-01&Dateto=2023-03-01`);
+  }
   
 }
