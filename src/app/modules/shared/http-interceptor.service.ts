7be2b818e086cea2534443347ad139517aeced99
devkhalil1337 <devkhalil1337@gmail.com>
2023-11-03 10:00:09 +0500

    error handler added in http-interceptor

--- src/app/modules/shared/http-interceptor.service.ts
+++ src/app/modules/shared/http-interceptor.service.ts
@@ -1,27 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { ToasterService } from './toaster.service';
 
 @Injectable()
 export class HttpInterceptorService implements HttpInterceptor {
-  constructor() { }
+  constructor(private toaster: ToasterService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     let token = localStorage.getItem("jwt");
     let businessId = localStorage.getItem("businessId");
-    let  modifiedRequest = request;
+    let modifiedRequest = request;
     if (token) {
       token = `Bearer ${token}`;
     }
-    if(!request.url.includes('CheckUserCredentials')){
+    if (!request.url.includes('CheckUserCredentials')) {
       modifiedRequest = request.clone({
         headers: request.headers
           .set("businessId", businessId)
           .set("Authorization", token)
       });
     }
-  
-    return next.handle(modifiedRequest);
+
+    return next.handle(modifiedRequest).pipe(
+      catchError((error: any) => {
+        // Handle HTTP errors here
+        if (error instanceof HttpErrorResponse) {
+          console.error('HTTP error:', error);
+          if (error?.error?.message) {
+            this.toaster.error(error?.error?.message)
+          }
+        }
+        return throwError(error); // Re-throw the error to propagate to the global error handler
+      })
+    );
   }
-  
+
 }
