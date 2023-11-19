4a62f7a5a7ecf737a149f199024645bea39d266c
devkhalil1337 <devkhalil1337@gmail.com>
2023-11-20 02:56:56 +0500

    hide and fix design changes

--- src/app/modules/backoffice/orders/orders.component.ts
+++ src/app/modules/backoffice/orders/orders.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ColDef, GridOptions } from 'ag-grid-community';
 import { DateFormats, GridColumnType, OrderStatus } from '@enums';
-import { UtilityService , ConfigService, ToasterService} from '@shared';
+import { UtilityService, ConfigService, ToasterService } from '@shared';
 import { OrdersService } from './orders.service';
 import { RouterlinkrendererComponent } from '../../shared/components/routerlinkrenderer/routerlinkrenderer.component';
 import { interval, Subscription } from 'rxjs';
@@ -17,27 +17,27 @@ import { DateRangeType } from 'src/app/enums/date-range';
 })
 
 
-export class OrdersComponent  implements OnInit {
+export class OrdersComponent implements OnInit {
 
   private statusSubscription: Subscription;
   gridOptions: GridOptions
   StatusTypes = OrderStatus
-  OrderFilter:OrderFilter
-  selectizeConfig:any;
-  
-  get isEditButtonEnable(){
-    if(this.gridOptions){
+  OrderFilter: OrderFilter
+  selectizeConfig: any;
+
+  get isEditButtonEnable() {
+    if (this.gridOptions) {
       const rows = this.gridOptions.api?.getSelectedRows();
       return rows && rows.length > 0;
     }
     return false;
   }
-  
 
-  constructor(private utils:UtilityService, private configService:ConfigService,
-    private ordersService:OrdersService,private toasterService:ToasterService) { 
+
+  constructor(private utils: UtilityService, private configService: ConfigService,
+    private ordersService: OrdersService, private toasterService: ToasterService) {
     this.OrderFilter = new OrderFilter();
-    this.OrderFilter.dateRange = new DateRange(DateRangeType.Last7Days,"0",0);
+    this.OrderFilter.dateRange = new DateRange(DateRangeType.Last7Days, "0", 0);
     this.selectizeConfig = this.configService.getSelectizeConfig(1);
     this.initGridConfig();
   }
@@ -47,24 +47,24 @@ export class OrdersComponent  implements OnInit {
   }
 
   ngOnDestroy(): void {
-    if(this.statusSubscription)
+    if (this.statusSubscription)
       this.statusSubscription.unsubscribe();
   }
 
-  getGridData(){
+  getGridData() {
     this.toggleGridOverlay(true);
-      this.ordersService.getAllOrders(this.OrderFilter).subscribe(response => {
-        this.utils.setGridData(this.gridOptions,response)
-        this.toggleGridOverlay(false);
-        this.checkOrderStatus();
-      },(error) => {
-        this.toggleGridOverlay(false);
-        this.toasterService.error(error);
-      })
+    this.ordersService.getAllOrders(this.OrderFilter).subscribe(response => {
+      this.utils.setGridData(this.gridOptions, response)
+      this.toggleGridOverlay(false);
+      this.checkOrderStatus();
+    }, (error) => {
+      this.toggleGridOverlay(false);
+      this.toasterService.error(error);
+    })
   }
 
-  private initGridConfig(){
-    this.gridOptions = this.configService.getGridConfig(false,true);
+  private initGridConfig() {
+    this.gridOptions = this.configService.getGridConfig(false, true);
     this.gridOptions.columnDefs = this.getGridColumnDefs();
     this.gridOptions.pagination = true;
     this.gridOptions.paginationPageSize = 10;
@@ -73,25 +73,40 @@ export class OrdersComponent  implements OnInit {
     }
   }
 
-  private toggleGridOverlay(showLoading:boolean = false):void{
-    this.utils.toggleGridOverlay(this.gridOptions,showLoading)
+  private toggleGridOverlay(showLoading: boolean = false): void {
+    this.utils.toggleGridOverlay(this.gridOptions, showLoading)
   }
 
 
   private getGridColumnDefs(): Array<ColDef> {
-     const headerColumn = this.configService.getCheckboxConfig();
-     headerColumn.headerClass = 'header_one';
-    return [
-      {
-        ...headerColumn
-      },{
+    const headerColumn = this.configService.getCheckboxConfig();
+    headerColumn.headerClass = 'header_one';
+    return [{
+      headerName: 'Id',
+      field: 'Id',
+      cellClass: "text-center",
+      headerClass: 'header_one',
+      minWidth: 100,
+      maxWidth: 100,
+      sortable: false,
+      valueGetter: params => params.node.rowIndex + 1,
+      type: GridColumnType.number
+    }, {
+      headerName: 'Order Number',
+      field: 'orderInvoiceNumber',
+      headerClass: 'header_one',
+      cellClass: "text-center",
+      sortable: false,
+      width: 100,
+      cellRenderer: RouterlinkrendererComponent
+    }, {
       headerName: 'Status',
-      headerClass:'header_one',
+      headerClass: 'header_one',
       field: 'orderStatus',
-      editable:true,
-      sortable:false,
+      editable: true,
+      sortable: false,
       onCellValueChanged: (params) => {
-        if(params.newValue != params.oldValue){
+        if (params.newValue != params.oldValue) {
           this.updateStatus(params)
         }
       },
@@ -104,8 +119,8 @@ export class OrdersComponent  implements OnInit {
         let statusTypes = this.StatusTypes;
         return { values: statusTypes.map(({ value }) => value) };
       },
-      cellRenderer: (params:any) => {
-        switch(params.value.toUpperCase()){
+      cellRenderer: (params: any) => {
+        switch (params.value.toUpperCase()) {
           case OrderStatusEnums.Open:
             return `<span class="badge badge-primary p-2 w-100">${params.value.toUpperCase()}</span>`;
           case OrderStatusEnums.OnTheWay:
@@ -116,60 +131,52 @@ export class OrdersComponent  implements OnInit {
             return `<span class="badge badge-success p-2 w-100">${params.value.toUpperCase()}</span>`;
           default:
             return `<span class="badge badge-primary p-2 w-100">${params.value.toUpperCase()}</span>`;
-          }
+        }
       },
-      maxWidth:150
+      maxWidth: 150
     },
     {
-      headerName: 'Order Number',
-      field: 'orderInvoiceNumber',
-      headerClass: 'header_one',
-      cellClass:"text-center",
-      sortable: false,
-      width:100,
-      cellRenderer:RouterlinkrendererComponent
-    },{
       headerName: 'Amount',
       field: 'totalAmount',
-      cellClass:"text-center",
-      width:100,
+      cellClass: "text-center",
+      width: 100,
       headerClass: 'header_one',
       sortable: false,
-      type:GridColumnType.currency
-    },{
+      type: GridColumnType.currency
+    }, {
       headerName: 'Date',
       field: 'createdDate',
-      cellClass:"text-center",
+      cellClass: "text-center",
       headerClass: 'header_one',
       sortable: false,
       sort: 'desc',
-      width:100,
+      width: 100,
       comparator: (valueA, valueB) => (valueA == valueB) ? 0 : (valueA > valueB) ? 1 : -1,
-      type:GridColumnType.dateTime
+      type: GridColumnType.dateTime
     }];
   }
 
 
 
-  updateStatus(params:any){
+  updateStatus(params: any) {
     const orderNumber = params.data.orderInvoiceNumber;
     const orderStatus = params.newValue;
-    this.ordersService.updateOrderStatus(orderNumber,orderStatus).subscribe(response => {
-      if(response){
+    this.ordersService.updateOrderStatus(orderNumber, orderStatus).subscribe(response => {
+      if (response) {
         this.toasterService.success(`Order Status has been updated to ${orderStatus}`)
       }
       this.getGridData();
     })
   }
 
-  checkOrderStatus(){
+  checkOrderStatus() {
     this.statusSubscription = interval(1500000) // Check every 15 seconds
-    .pipe(
-      switchMap(() => this.ordersService.getAllOrders(this.OrderFilter))
-    )
-    .subscribe(response => {
-      this.gridOptions.api?.setRowData(response);
-    });
+      .pipe(
+        switchMap(() => this.ordersService.getAllOrders(this.OrderFilter))
+      )
+      .subscribe(response => {
+        this.gridOptions.api?.setRowData(response);
+      });
 
   }
 
@@ -178,7 +185,7 @@ export class OrdersComponent  implements OnInit {
       if (dtRange.label === DateRangeType.customRange) {
         this.OrderFilter.dateRange = this.configService.getNewDateRange(dtRange.label, undefined, sDate, eDate);
       } else {
-        this.OrderFilter.dateRange = this.configService.getNewDateRange(dtRange.label,0);
+        this.OrderFilter.dateRange = this.configService.getNewDateRange(dtRange.label, 0);
       }
     } else {
       this.OrderFilter.dateRange = this.configService.getNewDateRange(DateRangeType.Last7Days);
@@ -188,12 +195,12 @@ export class OrdersComponent  implements OnInit {
   }
 
 
-  onSubmit(){
-    this.setDateRangeByBaseDate(this.OrderFilter.dateRange,this.OrderFilter.startDate,this.OrderFilter.endDate);
+  onSubmit() {
+    this.setDateRangeByBaseDate(this.OrderFilter.dateRange, this.OrderFilter.startDate, this.OrderFilter.endDate);
     this.getGridData();
   }
 
-  onDateChange($event:any){
+  onDateChange($event: any) {
     this.onSubmit();
   }
 
